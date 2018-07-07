7d2eccc9fa2fde8dd9a685b4443fff6263638e71
Victor Vieira <victorvieira89@gmail.com>
2018-07-06 21:44:59 -0300

    Creating drawable resources Implementing activity_main.xml and activity_login.xml

--- app/src/main/java/com/atwork/centralrn/activities/LoginActivity.java
+++ app/src/main/java/com/atwork/centralrn/activities/LoginActivity.java
@@ -1,13 +1,15 @@
-package com.atwork.centralrn;
+package com.atwork.centralrn.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.atwork.centralrn.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
